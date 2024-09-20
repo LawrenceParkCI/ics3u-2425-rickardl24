84bb9d67802139515986731665858f45f2d54a7d
342973682 <342973682@C-MXL2393F25.tdsb.on.ca>
2024-09-20 11:11:44 -0400

    Adding ICS3U project with HelloWorld class

---
+++ ICS3U-RickardL/src/unit1/HelloWorld.java
@@ -0,0 +1,20 @@
+package unit1;
+/**
+ * Description: This program prints Hello World, and a second statement. <p>
+ * Date: Sept. 19, 2024
+ * @author Liam Rickard
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// This prints hello to the system console
+		System.out.print("Hello World!!");
+		System.out.println("My name is Liam");
+
+		/*
+		 * This is a block comment
+		 * You can add notes on multiple lines
+		 */
+	}
+
+}
