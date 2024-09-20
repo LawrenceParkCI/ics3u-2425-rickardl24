15774b8eb63fb76fb2820f8705754dce862eac5f
342973682 <342973682@C-MXL2393F25.tdsb.on.ca>
2024-09-20 11:40:19 -0400

    This is about me. I showed my name, grade, teacher and school.

---
+++ ICS3U-RickardL/src/unit1/AboutMe.java
@@ -0,0 +1,19 @@
+package unit1;
+/**
+ * Description: This program prints my name, grade, teacher and school
+ * Date: Sept. 20, 2024
+ * @author Liam Rickard
+ */
+public class AboutMe {
+
+	public static void main(String[] args) {
+		// This prints hello to the system console
+		System.out.println("About Me");
+		System.out.println();
+		System.out.println("My name is Liam");
+		System.out.println("I am in grade 11");
+		System.out.println("My teacher is Ms. Kemp");
+		System.out.println("I go to Lawrence Park");
+	}
+
+}
