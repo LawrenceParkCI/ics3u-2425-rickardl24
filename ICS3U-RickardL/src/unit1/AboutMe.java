ea1e4bc86f81285edeb5808a928453e7e4512177
342973682 <342973682@C-MXL2393F25.tdsb.on.ca>
2024-09-24 11:39:18 -0400

    Worked on weekly schedule of organizing classes and class times.

--- ICS3U-RickardL/src/unit1/AboutMe.java
+++ ICS3U-RickardL/src/unit1/AboutMe.java
@@ -15,7 +15,21 @@ public class AboutMe {
 		System.out.println("My teacher is Ms. Kemp");
 		System.out.println("I go to Lawrence Park");
 		System.out.println("\"Go Panthers!\"");
-		System.out.println("Date: %-15sTimes: %8s","Monday, (9:00-10:20);
+		System.out.format("\n");
+		System.out.println("\n ______________________________________________________________________________________________________________________________________________");
+		System.out.format("\t Date: \t \t Period 1 \t \t Period 2 \t \t Period 3 \t \t Period 4");
+		System.out.println("\n \t \t    Computer Science \t \t Data Management \t   IAPS      \t \t Accounting");
+		System.out.format("\n \t Monday \t 9:00-10:20 \t \t 10:20-11:40 \t \t 12:40-1:55 \t \t 2:00-3:15");
+		System.out.println("\n");
+		System.out.format("\n \t Tuesday \t 9:00-10:20 \t \t 10:20-11:40 \t \t 12:40-1:55 \t \t 2:00-3:15");
+		System.out.println("\n");
+		System.out.format("\n \t Wednesday \t 9:55-10:55 \t \t 11:00-12:00 \t \t 1:00-2:05 \t \t 2:10-3:15");
+		System.out.println("\n");
+		System.out.format("\n \t Thursday \t 9:00-10:20 \t \t 10:20-11:40 \t \t 12:40-1:55 \t \t 2:00-3:15");
+		System.out.println("\n");
+		System.out.format("\n \t Friday \t 9:00-10:20 \t \t 10:20-11:40 \t \t 12:40-1:55 \t \t 2:00-3:15");
+		System.out.println("\n");
+		
 		
 		
 	}
