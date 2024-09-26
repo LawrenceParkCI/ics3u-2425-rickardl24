3444caf19cb46e9abf7c86110fc9bd9c017b7114
342973682 <342973682@C-MXL2393F25.tdsb.on.ca>
2024-09-26 11:37:40 -0400

    Started Bingo project starting the format and writing the rules.

--- ICS3U-RickardL/src/unit1/Bingo.java
+++ ICS3U-RickardL/src/unit1/Bingo.java
@@ -2,8 +2,17 @@ package unit1;
 
 public class Bingo {
 
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			System.out.format("Bingo Card");
+			System.out.format("\nRules");
+			System.out.println("\n 1. The Caller Pulls a Numbered Bingo Ball");
+			System.out.println("\n 2. The Number is Placed n the Bingo Board and Called Out");
+			System.out.println("\n 3. Players Look for the Called Number on Their Bingo Card");
+			System.out.println("\n 4. If the Number is Located, it is Market Off");
+			System.out.println("\n 5. Steps 1 to 4 are Repeated Until a Player Matches the BINGO Pattern");
+			System.out.println("\n 6. The Winning Player Yells BINGO.");
+			
 
 	}
 
