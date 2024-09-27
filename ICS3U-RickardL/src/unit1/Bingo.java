4b55c28388a9e0bb89ee6ff67893157b8b9011c6
342973682 <342973682@C-MXL2393F25.tdsb.on.ca>
2024-09-27 10:00:38 -0400

    Completed Bingo card

--- ICS3U-RickardL/src/unit1/Bingo.java
+++ ICS3U-RickardL/src/unit1/Bingo.java
@@ -1,7 +1,14 @@
 package unit1;
-
+/**
+ * Description: Create a Bingo card
+ * Date: Sept. 27, 2024
+ * @author Liam Rickard
+ */
+		
 public class Bingo {
-
+/**
+ * @param args unused
+ */
 
 	public static void main(String[] args) {
 			System.out.format("Bingo Card");
@@ -12,7 +19,12 @@ public class Bingo {
 			System.out.println("\n 4. If the Number is Located, it is Market Off");
 			System.out.println("\n 5. Steps 1 to 4 are Repeated Until a Player Matches the BINGO Pattern");
 			System.out.println("\n 6. The Winning Player Yells BINGO.");
-			
+			System.out.format("\n \n \tB\tI\tN\tG\tO");
+			System.out.format("\n \t4\t16\t33\t59\t66");
+			System.out.format("\n \t7\t25\t31\t46\t62");
+			System.out.format("\n \t1\t19\tFREE\t49\t71");
+			System.out.format("\n \t11\t29\t41\t53\t75");
+			System.out.format("\n \t14\t17\t39\t51\t64");
 
 	}
 
