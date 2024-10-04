ae3d4b1bfdc05695103e4f5d5d2c73923acbe5ca
342973682 <342973682@C-MXL2393F25.tdsb.on.ca>
2024-10-04 10:52:21 -0400

    Completed floating-point variables worksheet

--- ICS3U-RickardL/src/unit1/InputFP.java
+++ ICS3U-RickardL/src/unit1/InputFP.java
@@ -1,10 +1,33 @@
 package unit1;
-
+import java.util.Scanner;
 public class InputFP {
+public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Scanner sc = new Scanner (System.in);
 
-	}
+	double num1, num2; 
+	
+	System.out.println("Type in your first number and press <Enter>");
+	num1 = sc.nextDouble();
+
+	
+	System.out.println("Type in your second number and press <Enter>");
+	num2 = sc.nextDouble();
+
+
+	System.out.println();
+	System.out.println("The first number entered was "+ num1);
+	System.out.println("The second number entered was "+ num2);
+
+	System.out.println();
+	System.out.println("SUM");
+	double sum = num1+num2;
+	
+	System.out.println("The sum of your numbers is " + sum);
+	sc.close();
+
+
+
+}
 
 }
