46575bbdde742cdaa831ac314a5852891e6a2088
342973682 <342973682@C-MXL2393F25.tdsb.on.ca>
2024-10-16 11:17:31 -0400

    Finished Alive, second part of the assignment

--- ICS3U-RickardL/src/unit1/NetPay.java
+++ ICS3U-RickardL/src/unit1/NetPay.java
@@ -8,12 +8,17 @@ public class NetPay {
 
 	public static void main(String[] args) {
 		
+		//create variables
+		
 		int hours = 40;
 		double wage = 5.00;
 		double insurance = 2.00;
 		double tax = 0.22;
 		
+		//calculate NetPay
 		double NetPay = (hours*wage-insurance) - tax*(hours*wage-insurance);
+		
+		//create a display message
 		System.out.println("Your net pay is " + NetPay);
 
 	}
