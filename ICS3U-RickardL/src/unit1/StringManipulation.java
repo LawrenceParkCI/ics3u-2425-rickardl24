35f089fe97454108bf15863799b2e89fc4c1fc9f
342973682 <342973682@C-MXL2393F25.tdsb.on.ca>
2024-10-17 09:36:28 -0400

    Finished string manipulation sheet

--- ICS3U-RickardL/src/unit1/StringManipulation.java
+++ ICS3U-RickardL/src/unit1/StringManipulation.java
@@ -8,8 +8,20 @@ public class StringManipulation {
 		Scanner sc = new Scanner (System.in);
 		
 		System.out.println("Input a sentance and press <Enter>");
-		int sentence;
-		sentence = sc.nextInt();
+		String UserInput;
+		UserInput = sc.nextLine();
+		
+		int firstWord = UserInput.indexOf(" ");
+		int lastWord = UserInput.lastIndexOf(" ");
+		System.out.println(UserInput);
+		System.out.println(UserInput.toUpperCase());
+		System.out.println(UserInput.toLowerCase());
+		System.out.println(UserInput.length());
+		System.out.println(UserInput.substring(5, 6));
+		
+		System.out.println("The first word is " + UserInput.substring(0 , firstWord));
+		System.out.println("The last word is" + UserInput.substring(lastWord));
+		
 	}
 
 }
