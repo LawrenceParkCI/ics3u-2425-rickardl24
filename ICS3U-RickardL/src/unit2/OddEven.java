e41fd4569b89b0a2571b8ccebbcfb5283b5c47f2
342973682 <342973682@C-MXL2393F25.tdsb.on.ca>
2024-11-01 10:16:19 -0400

    Started OddEven

---
+++ ICS3U-RickardL/src/unit2/OddEven.java
@@ -0,0 +1,17 @@
+package unit2;
+
+public class OddEven {
+	import java.util.Scanner;
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n;
+		System.out.println("Insert an integer and press <Enter>");
+		n = sc.nextInt();
+		if (n%2 == 0) {System.out.println("Your number is even");
+		
+		}
+
+	}
+
+
