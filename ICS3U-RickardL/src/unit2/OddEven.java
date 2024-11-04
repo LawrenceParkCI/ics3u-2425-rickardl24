2141322de960040522236b492a488dc9fe802e84
342973682 <342973682@C-MXL2393F25.tdsb.on.ca>
2024-11-04 11:19:36 -0500

    Finished OddEven worksheet

--- ICS3U-RickardL/src/unit2/OddEven.java
+++ ICS3U-RickardL/src/unit2/OddEven.java
@@ -1,17 +1,27 @@
 package unit2;
 
+/**
+ * Description: OddEven assignment
+ * Date: November 4, 2024
+ * @author Liam Rickard
+ */
+import java.util.Scanner;
 public class OddEven {
-	import java.util.Scanner;
+
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		int n;
+		Scanner sc = new Scanner (System.in);
+		int num;
 		System.out.println("Insert an integer and press <Enter>");
-		n = sc.nextInt();
-		if (n%2 == 0) {System.out.println("Your number is even");
+		num = sc.nextInt();
 		
+		if (num %2 == 0) {
+			System.out.println("Your number is even");
+		}
+		else {
+			System.out.println("Your number is odd");
 		}
 
 	}
 
-
+}
